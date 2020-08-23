2168b82555f70344dfee96472acd262011ee1976
Rakesh443 <rk20035@gmail.com>
2020-08-23 11:21:41 +0530

    New Commit

--- app/src/main/java/com/example/firstapp/LoginActivity.kt
+++ app/src/main/java/com/example/firstapp/LoginActivity.kt
@@ -7,62 +7,47 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
-    lateinit var sharedPreferences: SharedPreferences
+   // lateinit var sharedPreferences: SharedPreferences
 
-    var isRemembered=false
+   // var isRemembered=false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        sharedPreferences=getSharedPreferences("SHARED_PREF", Context.MODE_PRIVATE)
-        isRemembered = sharedPreferences.getBoolean("Check",false)
-        val nam:String=nameId.text.toString()
+        //sharedPreferences=getSharedPreferences("SHARED_PREF", Context.MODE_PRIVATE)
+       // isRemembered = sharedPreferences.getBoolean("Check",false)
+//        logId.setClickable(false)
+       // check()
+
+//        if(isRemembered){
+//            val  intent=Intent(this, AfterLoginActivity::class.java)
+//            startActivity(intent)
+//            finish()
+//        }
+
+    }
+
+    fun login(view: View) {
+        val name:String=nameId.text.toString()
         val pas:String=passId.text.toString()
-
-
-        if(isRemembered){
-            val  intent=Intent(this, AfterLoginActivity::class.java)
-            startActivity(intent)
-            finish()
+//        val editor:SharedPreferences.Editor=sharedPreferences.edit()
+//        editor.putString("Name",name)
+//        editor.putBoolean("Check",true)
+//        editor.apply()
+        if(name.equals(pas.reversed()) && !name.isNullOrBlank()){
+        val  intent=Intent(this, MusicActivity::class.java)
+        startActivity(intent)}
+        else{
+            Toast.makeText(applicationContext,"Invalid UserName and Password, Contact Admin",Toast.LENGTH_LONG).show()
         }
-
-
-
-
-        logId.setOnClickListener {
-            val name:String=nameId.text.toString()
-
-            val editor:SharedPreferences.Editor=sharedPreferences.edit()
-            editor.putString("Name",name)
-            editor.putBoolean("Check",true)
-            editor.apply()
-            val  intent=Intent(this, MusicActivity::class.java)
-            startActivity(intent)
-
-        }
-
     }
 
-
-    private val textWatcher=object :TextWatcher{
-        override fun afterTextChanged(p0: Editable?) {
-            TODO("Not yet implemented")
-        }
-
-        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-            TODO("Not yet implemented")
-        }
-
-        override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-            TODO("Not yet implemented")
-           // if()
-        }
-
-    }
 
 }
